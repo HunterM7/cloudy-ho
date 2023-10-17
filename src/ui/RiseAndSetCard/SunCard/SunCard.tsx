905116c630bfa4730977d4811cba9a4b99a3728c
Anton Meshchaninov <tonymsnv@gmail.com>
2023-10-17 12:41:09 +0300

    Created SunCard

---
+++ src/ui/RiseAndSetCard/SunCard/SunCard.tsx
@@ -0,0 +1,33 @@
+import { FC, HTMLAttributes } from 'react'
+import {
+  LightModeRounded as LightModeRoundedIcon,
+  DarkModeRounded as DarkModeRoundedIcon,
+} from '@mui/icons-material'
+import cn from 'classnames'
+
+// Styles
+import styles from './SunCard.module.scss'
+
+export interface SunCardProps extends HTMLAttributes<HTMLDivElement> {
+  type: 'sunrise' | 'sunset'
+  time: string
+}
+
+export const SunCard: FC<SunCardProps> = ({
+  type,
+  time,
+  className,
+  ...rest
+}) => {
+  return (
+    <div className={cn(styles.card, className)} {...rest}>
+      {type === 'sunrise' && <LightModeRoundedIcon className={styles.icon} />}
+      {type === 'sunset' && <DarkModeRoundedIcon className={styles.icon} />}
+
+      <p className={styles.title}>{type}</p>
+      <p className={styles.value}>{time}</p>
+    </div>
+  )
+}
+
+export default SunCard
