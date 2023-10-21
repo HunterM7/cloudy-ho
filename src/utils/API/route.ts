d0d6ee8040b541faa2ed964ddada7a030acacf05
Anton Meshchaninov <tonymsnv@gmail.com>
2023-10-21 14:49:53 +0300

    Commented all conflicted code

--- src/utils/API/route.ts
+++ src/utils/API/route.ts
@@ -1,47 +1,47 @@
-import { updateWeather, error404 } from './app.ts'
+// import { updateWeather, error404 } from './app.ts'
 
-const defaultLocation = '#/weather?lat=55.4507&lon=37.3656' // Moscow
+// const defaultLocation = '#/weather?lat=55.4507&lon=37.3656' // Moscow
 
-function currentLocation() {
-  window.navigator.geolocation.getCurrentPosition(
-    (res) => {
-      const { latitude, longitude } = res.coords
+// function currentLocation() {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (res) => {
+//       const { latitude, longitude } = res.coords
 
-      updateWeather(`lat=${latitude}`, `lon=${longitude}`)
-    },
-    (err) => {
-      window.location.hash = defaultLocation
-    },
-  )
-}
+//       updateWeather(`lat=${latitude}`, `lon=${longitude}`)
+//     },
+//     (err) => {
+//       window.location.hash = defaultLocation
+//     },
+//   )
+// }
 
-/**
- * @param {string} query Searched query
- */
-const searchedLocation = (query: string) => updateWeather(...query.split('&'))
-// updateWeather("lat=51.5073219", "lon=-0.1276474")
+// /**
+//  * @param {string} query Searched query
+//  */
+// const searchedLocation = (query: string) => updateWeather(...query.split('&'))
+// // updateWeather("lat=51.5073219", "lon=-0.1276474")
 
-const routes = new Map([
-  ['/current-location', currentLocation],
-  ['/weather', searchedLocation],
-])
+// const routes = new Map([
+//   ['/current-location', currentLocation],
+//   ['/weather', searchedLocation],
+// ])
 
-const checkHash = function () {
-  const requestURL = window.location.hash.slice(1)
+// const checkHash = function () {
+//   const requestURL = window.location.hash.slice(1)
 
-  const [route, query] = requestURL.includes
-    ? requestURL.split('?')
-    : [requestURL]
+//   const [route, query] = requestURL.includes
+//     ? requestURL.split('?')
+//     : [requestURL]
 
-  routes.get(route) ? routes.get(route)(query) : error404()
-}
+//   routes.get(route) ? routes.get(route)(query) : error404()
+// }
 
-window.addEventListener('hashchange', checkHash)
+// window.addEventListener('hashchange', checkHash)
 
-window.addEventListener('load', function () {
-  if (!window.location.hash) {
-    window.location.hash = '#/current-location'
-  } else {
-    checkHash()
-  }
-})
+// window.addEventListener('load', function () {
+//   if (!window.location.hash) {
+//     window.location.hash = '#/current-location'
+//   } else {
+//     checkHash()
+//   }
+// })
